088b25bf55795d13ae31f68743d1cc95eb713155
Txemi-CM <txemi@operamania.com>
2012-10-19 15:01:30 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Digren::Application.config.secret_token = 'e31ccce2978132d5d32da310c9f7cbc1b574c6a810be35118ed47f513eb482d2bfd84beb7bbfe87038412877f3eb8f3f2e91e00e39702eec6092981e8ce2bc87'
